c0b7c2e6cfdf645cfa6a90d0636598db4c4501e5
chennammal2003 <chennammal@firstmetainfra.com>
2026-08-31 07:44:59 +0530

    debug: Sanitize database URL for project ref check

--- server/server.js
+++ server/server.js
@@ -83,8 +83,7 @@ app.use((req, res, next) => {
 
 app.get("/api/db-check", (req, res) => {
   res.json({
-    database_url_host: process.env.DATABASE_URL ? process.env.DATABASE_URL.split('@')[1] : null,
-    direct_url_host: process.env.DIRECT_URL ? process.env.DIRECT_URL.split('@')[1] : null,
+    database_url: process.env.DATABASE_URL ? process.env.DATABASE_URL.replace(/:[^:@]+@/, '@') : null,
   });
 });
 
